60f4f9c489c98283207d07782ec5ac1a00027c1f
piavik <piavik@gmail.com>
2024-03-06 13:23:33 +0100

    CORS

--- main.py
+++ main.py
@@ -14,14 +14,26 @@ app = FastAPI()
 app.include_router(routes.router, prefix='/api')
 app.include_router(auth.router, prefix='/auth')
 
+cors_origins = [ 
+    "*"
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
-async def startup():
+async def startup() -> None:
     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0, encoding="utf-8", decode_responses=True)
     await FastAPILimiter.init(r)
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"message": "GoIT homework #11-13 - REST API via FastAPI"}
 
 
